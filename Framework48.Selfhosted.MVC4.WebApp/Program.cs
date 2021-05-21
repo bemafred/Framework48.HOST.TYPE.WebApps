e60c2e7cf914369c3420a496fc936eb91594a27a
Martin Fredriksson <mf@canyala.se>
2021-05-21 15:11:00 +0200

    Added some comments.

--- Framework48.Selfhosted.MVC4.WebApp/Program.cs
+++ Framework48.Selfhosted.MVC4.WebApp/Program.cs
@@ -13,13 +13,14 @@ namespace Framework48.Selfhosted.MVC4.WebApp
         {
             Console.WriteLine("Hello from .NET Framework 4.8 & MVC4");
 
+            // TODO: Extract virtual directory from assembly path so it will work no matter where it happen to be located.
             const string virtualDir = @"D:\src\git\bemafred\Framework48.HOST.TYPE.WebApps\Framework48.Selfhosted.MVC4.WebLib\";
-            const string relativePath = "/";
+            const string relativePath = ""; // No leading '/' please...
             const int port = 8080;
 
             AppHost.HostAndSetup<MvcApplication>(virtualDir, relativePath, port);
 
-            Console.WriteLine($"Listening on http://localhost:{port}{relativePath}");
+            Console.WriteLine($"Listening on http://localhost:{port}/{relativePath}");
         }
     }
 }
